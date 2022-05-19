bc0bbea985fa5aa16a5496a9c393b60454768524
diego <diego.cortinagarcia@gmail.com>
2022-05-19 13:30:31 +0200

    more examples

--- src/clase2/TestArray.java
+++ src/clase2/TestArray.java
@@ -1,10 +1,12 @@
 package clase2;
 
 public class TestArray {
+	static int[] misNros = { 3, 6, 9, 12 };
 
 	public static void main(String[] args) {
 		TestArray.ejemplo1();
 		TestArray.ejemplo2();
+		TestArray.ejemplo3();
 
 	}
 
@@ -22,9 +24,21 @@ public class TestArray {
 				System.out.println(items[i]);
 			}
 		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Demasiadas cosasen el armario");
+			System.out.println("Demasiadas cosas en el armario");
 		} catch (Exception e) {
 			System.out.println("Error");
+		}
+	}
+
+	static void ejemplo3() {
+		int[] nros = { 1, 2, 3 };
+		int[] valores = null;
+		valores[0] = 4;
+		valores[1] = 5;
+		valores[2] = 6;
+//uso de un array con alcance global para todos los metodos(en este caso estaticos)
+		for (int i = 0; i < misNros.length; i++) {
+			System.out.println(misNros[i]);
 		}
 	}
 }
