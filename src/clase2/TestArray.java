c5c27cd8c91029d01ef36c600e21cae53e8d4a04
diego <diego.cortinagarcia@gmail.com>
2022-05-19 13:13:37 +0200

    added try cath examples

--- src/clase2/TestArray.java
+++ src/clase2/TestArray.java
@@ -3,15 +3,28 @@ package clase2;
 public class TestArray {
 
 	public static void main(String[] args) {
-		ejemplo1();
 		TestArray.ejemplo1();
-		
+		TestArray.ejemplo2();
+
 	}
 
 	static void ejemplo1() {
 		String[] items = { "Shirt", "Trousers", "Scarf" };
 		for (int i = 0; i < items.length; i++) {
 			System.out.println(items[i]);
+		}
+	}
+
+	static void ejemplo2() {
+		String[] items = { "Shirt", "Trousers", "Scarf" };
+		try {
+			for (int i = 0; i <= items.length; i++) {
+				System.out.println(items[i]);
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Demasiadas cosasen el armario");
+		} catch (Exception e) {
+			System.out.println("Error");
 		}
 	}
 }
