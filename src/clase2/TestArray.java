71a7045dad453c656b7b4399773d992ecfe69d36
diego <diego.cortinagarcia@gmail.com>
2022-05-19 13:06:12 +0200

    added array class

---
+++ src/clase2/TestArray.java
@@ -0,0 +1,17 @@
+package clase2;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+	static void ejemplo1() {
+		String[] items = { "Shirt", "Trousers", "Scarf" };
+		for (int i = 0; i < items.length; i++) {
+			System.out.println(items[i]);
+		}
+	}
+}
